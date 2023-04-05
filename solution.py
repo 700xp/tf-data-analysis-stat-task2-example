a3a3fe3e6ae3192e16775a5c3fc10982fef7c649
Pavel Gogolev <122290372+700xp@users.noreply.github.com>
2023-04-05 18:56:34 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,7 +7,13 @@ from scipy.stats import chi2
 chat_id = 689327667 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
+    alpha = 1 - p
     stat = (x ** 2).sum()
-    left_edge_chi_squared = chi2.ppf(1 - p, 2 * len(x) - 1)
-    sigma_edge = np.sqrt(stat / (31 * left_edge_chi_squared))
-    return (-sigma_edge, sigma_edge)
+    
+    left_edge_chi_squared = chi2.ppf(alpha / 2, 2 * len(x) - 1)
+    right_edge_chi_squared = chi2.ppf(1 - alpha / 2, 2 * len(x) - 1)
+    
+    left_edge_sigma = np.sqrt(stat / (31 * right_edge_chi_squared))
+    right_edge_sigma = np.sqrt(stat / (31 * left_edge_chi_squared))
+    
+    return (left_edge_sigma, right_edge_sigma)
